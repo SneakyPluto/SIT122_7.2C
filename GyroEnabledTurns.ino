b151c35f5d74027af65508652b4daca965ac68ac
Travis <162380818+SneakyPluto@users.noreply.github.com>
2024-05-06 11:34:56 +1000

    Create GyroEnabledTurns.ino

---
+++ GyroEnabledTurns.ino
@@ -0,0 +1,150 @@
+#include <MeAuriga.h>
+#include <Wire.h>
+
+#define ALLLEDS 0
+
+// Auriga on-board light ring has 12 LEDs
+#define LEDNUM 12
+
+// on-board LED ring, at PORT0 (onboard)
+MeRGBLed led(0, LEDNUM);
+
+MeEncoderOnBoard Encoder_1(SLOT1);
+MeEncoderOnBoard Encoder_2(SLOT2);
+int16_t moveSpeed = 75;
+MeGyro gyro(1, 0x69);
+
+float j, f, k;
+
+// Threshold to determine light source direction
+const int threshold = 6;  // Adjust as needed based on your environment
+
+void Forward(void) {
+  Encoder_1.setMotorPwm(-moveSpeed);  // setMotorPwm writes to the encoder controller
+  Encoder_2.setMotorPwm(moveSpeed);   // so setting the speed change instantly
+}
+void Backward(void) {
+  Encoder_1.setMotorPwm(moveSpeed);
+  Encoder_2.setMotorPwm(-moveSpeed);
+}
+
+void BackwardAndTurnLeft(void) {
+  Encoder_1.setMotorPwm(moveSpeed / 4);
+  Encoder_2.setMotorPwm(-moveSpeed);
+}
+
+void BackwardAndTurnRight(void) {
+  Encoder_1.setMotorPwm(moveSpeed);
+  Encoder_2.setMotorPwm(-moveSpeed / 4);
+}
+
+void TurnLeft(void) {
+  Encoder_1.setMotorPwm(-moveSpeed);
+  Encoder_2.setMotorPwm(moveSpeed / 2);
+}
+
+void TurnRight() {
+  Encoder_1.setMotorPwm(-moveSpeed / 2);
+  Encoder_2.setMotorPwm(moveSpeed);
+}
+
+void TurnLeft1(void) {
+  Encoder_1.setMotorPwm(-moveSpeed);
+  Encoder_2.setMotorPwm(-moveSpeed);
+}
+
+void TurnRight1(void) {
+  Encoder_1.setMotorPwm(moveSpeed);
+  Encoder_2.setMotorPwm(moveSpeed);
+}
+
+void Stop(void) {
+  Encoder_1.setMotorPwm(0);
+  Encoder_2.setMotorPwm(0);
+}
+
+void ChangeSpeed(int16_t spd) {
+  moveSpeed = spd;
+}
+
+void setup() {
+  Serial.begin(115200);
+  //Set PWM 8KHz
+  TCCR1A = _BV(WGM10);
+  TCCR1B = _BV(CS11) | _BV(WGM12);
+  TCCR2A = _BV(WGM21) | _BV(WGM20);
+  TCCR2B = _BV(CS21);
+  gyro.begin();
+
+  // Initalise LED
+  led.setpin(44);
+
+  // turnRight(350);
+  // turnLeft(350);
+}
+
+void loop() {
+
+  // // Turn 360 degrees clockwise
+  led.setColor(0, 50, 0, 0);
+  led.show();
+  turnRight(345);
+  delay(2000);
+  led.setColor(0, 0, 0, 0);
+  led.show();
+  delay(3000);
+
+  // // Turn 360 degrees counter-clockwise
+  led.setColor(0, 0, 50, 0);
+  led.show();
+  turnLeft(345);
+  delay(2000);
+  led.setColor(0, 0, 0, 0);
+  led.show();
+  delay(3000);
+}
+
+
+void turnRight(double degree) {
+  gyro.update();
+  Serial.println(gyro.getAngleZ());
+  double startAngle = gyro.getAngleZ() + 180.00;
+  double delta;
+  TurnRight1();
+  while (degree > 0) {
+    gyro.update();
+    delta = gyro.getAngleZ() + 180.00 - startAngle;
+    if (delta < -180.00) {
+      delta += 360.00;
+    } else if (delta > 180) {
+      delta -= 360;
+    }
+    degree -= delta;
+    startAngle = gyro.getAngleZ() + 180.00;
+  }
+  gyro.update();
+  Serial.println(gyro.getAngleZ());
+  Stop();
+}
+
+void turnLeft(double degree) {
+  gyro.update();
+  Serial.println(gyro.getAngleZ());
+  double startAngle = gyro.getAngleZ() + 180.00;
+  double delta;
+  TurnLeft1();
+  while (degree > 0) {
+    gyro.update();
+    delta = gyro.getAngleZ() + 180.00 - startAngle;
+    if (delta < -180.00) {
+      delta += 360.00;
+    } else if (delta > 180) {
+      delta -= 360;
+    }
+    degree += delta;
+    startAngle = gyro.getAngleZ() + 180.00;
+  }
+  gyro.update();
+  Serial.println(gyro.getAngleZ());
+  Stop();
+}
